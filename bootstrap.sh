4605a08b326b562161a93c55b8dd86710de3537a
Jamorham <jamorham@users.noreply.github.com>
2022-08-30 18:45:28 +0000

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+
+echo 
+echo "Bootstrapping Nightscout installer - JamOrHam"
+echo
+cd /tmp
+if [ ! -s installation.sh ]
+then
+sudo apt-get update
+sudo apt-get -y install wget bash
+wget https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/installation.sh
+if [ ! -s installation.sh ]
+then
+echo "UNABLE TO DOWNLOAD INSTALLATION SCRIPT! - cannot continue - please try again!"
+exit 5
+fi
+fi
+echo
+echo "Running installer"
+echo
+sudo bash installation.sh
+echo
+echo "Finished running installer"
+echo
+
+
