6ad4db18ac3c50a926d8753c0c6d282a9c2d59a7
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-09 22:37:33 -0400

    Add files via upload
    Exit if any of the fork parameters are left empty.
    Every exit has an echo now.
    Removed tar extraction.

--- update_nightscout.sh
+++ update_nightscout.sh
@@ -21,7 +21,7 @@ mkswap /var/SWAP
 fi
 swapon 2>/dev/null /var/SWAP
 
-echo "Installing system basics"
+echo "Installing dialog"
 sudo apt-get update
 sudo apt-get -y install dialog
 
@@ -33,7 +33,7 @@ user="nightscout"
 repo="cgm-remote-monitor"
 brnch="master"
 
-clear #  Clear the screen before placing the next dialog one.
+clear #  Clear the screen before placing the next dialog on.
 dialog --yesno "You can reeinstall Nightscout.\n\n
 Choose Yes to install the latest version of official Nightscout.\n\n
 Choose No to install from a fork you can specify (advanced).\n\n
@@ -42,10 +42,12 @@ ans=$?
 if [ $ans = 255 ] # Exit if escape is pressed.
 then
 clear # Clear the screen before exiting.
+echo "Escape"
+echo "Cannot continue."
 exit 5
 elif [ $ans = 1 ] # We need Github details
 then
-# So, let's clear these first.
+# Clear fork parameters.
 user=""
 repo=""
 brnch=""
@@ -53,14 +55,15 @@ brnch=""
 # open fd
 exec 3>&1
 
-# Now, let's ask for the details of the fork
-# Store data to $VALUES variable
+# Ask for the fork details. 
 clear # Clear the screen before placing the next dialog on.
 VALUES=$(dialog --ok-label "Submit" --form "Enter the GitHub details for the Nightscout version you want to install.\n" 12 50 0 "User ID:" 1 1 "$user" 1 14 25 0 "Repository:" 2 1 "$repo" 2 14 25 0 "Branch:" 3 1 "$brnch" 3 14 25 0 2>&1 1>&3)
 ans2=$?
 if [ $ans2 = 255 ] || [ $ans2 = 1 ] # Exit if escaped or cancelled
 then
-clear # Clear the screen before existing.
+clear # Clear dialog.
+echo "Escape or Cancel"
+echo "Cannot continue."
 exit 5
 fi
 
@@ -72,6 +75,13 @@ exec 3>&-
 user=$(echo "$VALUES" | sed -n 1p)
 repo=$(echo "$VALUES" | sed -n 2p)
 brnch=$(echo "$VALUES" | sed -n 3p)
+if [ "$user" = "" ] || [ "$repo" = "" ] || [ "$brnch" = "" ]
+then
+clear # clear before exiting
+echo "Missing fork parameters"
+echo "Cannot continue."
+exit 5
+fi
 fi
 clear  # Clear the last dialog
 
@@ -98,15 +108,6 @@ do
 read -t 0.1 dummy
 done
 
-if [ ! -s /usr/local/etc/no-ip2.conf ]
-then
-cd /usr/src
-sudo tar -xzf /srv/nightscout-vps/helper/noip-duc-linux.tar.gz
-cd /usr/src/noip-2.1.9-1
-sudo make install
-else
-echo "Noip client already installed - delete /usr/local/etc/no-ip2.conf if you want to change config"
-fi
 noip2 -S
 
 hostname=`noip2 -S 2>&1 | grep host | tr -s ' ' | tr -d '\t' | cut -f2 -d' ' | head -1`
