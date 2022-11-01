66ca7091bf32db5d8b59d51625e945ee55dbeffe
root <root@instance-7.us-west4-b.c.fresh-rampart-361520.internal>
2022-11-01 16:49:57 +0000

    Minor Fixes.

--- ConfigServer/run_server_linux.sh
+++ ConfigServer/run_server_linux.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
+if [ "`id -u`" != "0" ]
+then
+echo "Script needs root - pleass run sudo ./run_server_linux.sh"
+echo "Cannot continue.."
+exit 5
+fi
+
+
+
+
+
 export SECRET_KEY=$(uuidgen)
-export ENV_DEBUG=False 
+export ENV_DEBUG=False
 export ENV_TOKEN=$(uuidgen)
 export NS_CONFIG_FILE=/etc/nsconfig
 
-python3 manage.py migrate
-
+sudo apt install python3-pip
+pip install Django
 pip install django-extensions Werkzeug
+
+
+python3 manage.py migrate  >> /tmp/variables_log 2>&1
 
 . /etc/free-dns.sh
 CERT_LOCATION="/etc/letsencrypt/live/"$HOSTNAME
 
-echo PLEASE CONNECT TO:
-echo https://$HOSTNAME:3389/variables?token=$ENV_TOKEN
-
+echo
+echo PLEASE CONNECT TO https://$HOSTNAME:3389/variables?token=$ENV_TOKEN
+echo
 
 #python3 manage.py runserver 0.0.0.0:3389
-python3 manage.py runserver_plus 0.0.0.0:3389 --cert-file $CERT_LOCATION/cert.pem --key-file $CERT_LOCATION/privkey.pem
+python3 manage.py runserver_plus 0.0.0.0:3389 --cert-file $CERT_LOCATION/cert.pem --key-file $CERT_LOCATION/privkey.pem >> /tmp/variables_log 2>&1
 
