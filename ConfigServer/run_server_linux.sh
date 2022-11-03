92e21be8285a1cd5a575715d95bf9592038ebff6
Tzachi Dar <tzachi.dar@gmail.com>
2022-11-03 21:24:33 +0200

    Add environment variable to stop the server automatically after 15 minutes of inactivity.

--- ConfigServer/run_server_linux.sh
+++ ConfigServer/run_server_linux.sh
@@ -19,13 +19,16 @@ pip install django-extensions Werkzeug
 
 python3 manage.py migrate  >> /tmp/variables_log 2>&1
 
+#make sure to put this after the migrate, as the migrate might fail.
+export KILL_AFTER_IDLE_TIME=900
+
 . /etc/free-dns.sh
 CERT_LOCATION="/etc/letsencrypt/live/"$HOSTNAME
 
 echo
 echo PLEASE CONNECT TO https://$HOSTNAME:3389/variables?token=$ENV_TOKEN
+echo "The server will run for 15 minutes, and after that will stop (if not used). Press ctrl C to stop it before that."
 echo
 
-#python3 manage.py runserver 0.0.0.0:3389
+#python3 manage.py runserver 0.0.0.0:3389  >> /tmp/variables_log 2>&1
 python3 manage.py runserver_plus 0.0.0.0:3389 --cert-file $CERT_LOCATION/cert.pem --key-file $CERT_LOCATION/privkey.pem >> /tmp/variables_log 2>&1
-
