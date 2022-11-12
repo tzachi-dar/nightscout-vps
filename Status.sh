fcd3a0ddeeee8bf5ae7a727f17e6f88d32d1c2fb
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-12 14:53:15 -0500

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -6,14 +6,15 @@ echo "Please be patient (30 seconds)"
 echo "  "
 echo "  "
 
-Zone=$(basename `curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"`)
+ZoneRaw=$(basename `curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"`)
+Zone="$ZoneRaw"
 basename `curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"` > /tmp/Zone
 grep 'us-west1' /tmp/Zone > /tmp/us-west1
 grep 'us-central1' /tmp/Zone > /tmp/us-central1
 grep 'us-east1' /tmp/Zone > /tmp/us-east1
 if [ ! -s /tmp/us-west1 ] && [ ! -s /tmp/us-central1 ] && [ ! -s /tmp/us-east1 ] 
 then
-Zone="\Zb\Z1 $Zone "
+Zone="\Zb\Z1 $ZoneRaw \Zn"
 fi
 
 Ram=$(free -m | sed -n 2p | awk '{print $2}')
