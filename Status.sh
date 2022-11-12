65bf028e3df423027f7d59e1b49532f82893d6cb
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-12 16:28:01 -0500

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -6,6 +6,7 @@ echo "Please be patient (30 seconds)"
 echo "  "
 echo "  "
 
+# Virtual machine zone
 ZoneRaw=$(basename `curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"`)
 Zone="$ZoneRaw"
 basename `curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"` > /tmp/Zone
@@ -14,10 +15,10 @@ grep 'us-central1' /tmp/Zone > /tmp/us-central1
 grep 'us-east1' /tmp/Zone > /tmp/us-east1
 if [ ! -s /tmp/us-west1 ] && [ ! -s /tmp/us-central1 ] && [ ! -s /tmp/us-east1 ] 
 then
-echo "here"
 Zone="\Zb\Z1"$ZoneRaw"\Zn"
 fi
 
+# Ram size - This is used to determine if the machine type is micro or not
 Ram=$(free -m | sed -n 2p | awk '{print $2}')
 unit="M"
 Ramsize="$Ram"$unit
@@ -26,26 +27,32 @@ then
 Ramsize="\Zb\Z1 $Ram$unit \Zn"
 fi
 
+# Disk type
 disk="\Z1\ZbBalanced\Zn"
 if [ $(cat /sys/block/sda/queue/rotational) -eq 1 ]
 then 
 disk="Standard"
 fi
 
+#Disk size
 disksz="$(df -h | sed -n 2p | awk '{print $2}')"
+DiskUsedPercent="$(df -h | sed -n 2p | awk '{print $5}')"
 if [ ! "$disksz" = "29G" ]
 then
 disksz="\Zb\Z1$(df -h | sed -n 2p | awk '{print $2}')\Zn"
 fi
 
+#Swap file
 swap="$(free -h | sed -n 3p | awk '{print $2}')"
 
+#Ubuntu version
 ubuntu="$(lsb_release -a | sed -n 2p | awk '{print $3, $4}')"
 if [ ! "$ubuntu" = "20.04.5 LTS" ]
 then
 ubuntu="\Zb\Z1$(lsb_release -a | sed -n 2p | awk '{print $3, $4}')\Zn"
 fi
 
+#Firewall
 current=$(wget -q -O - http://checkip.dyndns.org|sed s/[^0-9.]//g)
 nc -zvw10 $current 80 2> /tmp/http
 grep 'timed out' /tmp/http > /tmp/http2
@@ -67,7 +74,7 @@ dialog --colors --msgbox "       \Zr Developed by the xDrip team \Zn\n\n\
 Zone: "$Zone" \n\
 RAM: $Ramsize \n\
 Disk type: "$disk" \n\
-Disk size: $disksz \n\
+Disk size: $disksz    $DiskUsedPercent used \n\
 Ubuntu: $ubuntu \n\
 HTTP & HTTPS:  $http \n\n\
 ------------------------------------------ \n\
