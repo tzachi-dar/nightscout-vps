bdedc08c5dc3c1c72ee3db80bfe50d0b69bf730b
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-12 22:43:22 -0500

    Update update_nightscout.sh

--- update_nightscout.sh
+++ update_nightscout.sh
@@ -10,8 +10,7 @@ if [ "$a" = ""  ]
 then
 clear
 dialog --colors --msgbox "     \Zb\Z1 Developed by the xDrip team \zn\n\n\
-You have not completed the first installation phase.\n\
-You need to complete that first." 10 51
+You need to complete installation phase 1 first." 9 50
 exit
 fi
 
@@ -29,7 +28,7 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
       \Zr Developed by the xDrip team \Zn\
   \n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 13 50 3\
+Press Enter to choose the highlighted option.\n\n" 13 50 3\
  "1" "Install latest official Nightscout"\
  "2" "Install from a fork (Advanced)"\
  "3" "Return to main menu"\
@@ -89,62 +88,12 @@ esac
 
 done
 
-
-#dialog --colors --yesno "     \Zr Developed by the xDrip team \Zn\n\n\
-#Reinstall official Nightscout?\n\n
-#Choose Yes to install the latest version of official Nightscout.\n\n
-#Choose No to install from a fork (advanced).\n\n
-#Or, press escape to cancel." 16 50
-#ans=$?
-#if [ $ans = 255 ] # Exit if escape is pressed.
-#then
-#clear # Clear the screen before exiting.
-#echo "Escape"
-#echo "Cannot continue."
-#exit 5
-#elif [ $ans = 1 ] # We need Github details
-#then
-## Clear fork parameters so that we can detect user not entering them all.
-#user=""
-#repo=""
-#brnch=""
-#
-## open fd
-#exec 3>&1
-#
-## Ask for the fork details. 
-#clear # Clear the screen before placing the next dialog on.
-#VALUES=$(dialog --colors --ok-label "Submit" --form "     \Zr Developed by the xDrip team \Zn\n\n Enter the GitHub details for the Nightscout version you want to install.\n" 14 50 0 "User ID:" 1 1 "$user" 1 14 25 0 "Repository:" 2 1 "$repo" 2 14 25 0 "Branch:" 3 1 "$brnch" 3 14 25 0 2>&1 1>&3)
-#ans2=$?
-#if [ $ans2 = 255 ] || [ $ans2 = 1 ] # Exit if escaped or cancelled
-#then
-#clear # Clear dialog.
-#echo "Escape or Cancel"
-#echo "Cannot continue."
-#exit 5
-#fi
-#
-## close fd
-#exec 3>&-
-#
-## Assign the entered values to corresponding parameters 
-#user=$(echo "$VALUES" | sed -n 1p)
-#repo=$(echo "$VALUES" | sed -n 2p)
-#brnch=$(echo "$VALUES" | sed -n 3p)
-#if [ "$user" = "" ] || [ "$repo" = "" ] || [ "$brnch" = "" ] # Abort if either paramter was left blank. 
-#then
-#clear # clear before exiting
-#echo "Missing fork parameters"
-#echo "Cannot continue."
-#exit 5
-#fi
-#fi
 clear  # Clear the last dialog
 
 # Last chance to stop before any destructive actions.
 dialog --colors --msgbox "    \Zr Developed by the xDrip team \Zn\n\n
-The install will take about 28 minutes.  Nightscout will be down in that time.  You can cancel by pressing escape now.\n\n\
-You can minimize this terminal.  But, please don't close it.  After completion, the server will reboot." 13 50
+The installation will take about 28 minutes.  Nightscout will be down in that time.  You can cancel by pressing escape now.\n\n\
+If you procedd, you can minimize this terminal.  But, please don't close it.  After completion, the server will reboot." 13 50
 response=$?
 if [ $response = 255 ]
 then
