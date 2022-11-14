a6e9393279db291b0ceb0993d4f30c1cc388a804
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-13 22:48:44 -0500

    Update varserver.sh

--- varserver.sh
+++ varserver.sh
@@ -5,3 +5,4 @@ You can use this utility only if you use FreeDNS.\n\n\
 Copy the URL shown on the following page.  Paste it into a web browser.  You will have 15 minutes to edit your variables on that browser." 11 50
 
 sudo /xDrip/ConfigServer/run_server_linux.sh
+ 
