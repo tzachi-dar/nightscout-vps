f1bf280447fe5682e77b63d02cc8d67109493739
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-13 23:00:36 -0500

    Update varserver.sh

--- varserver.sh
+++ varserver.sh
@@ -2,7 +2,7 @@
 
 dialog --colors --msgbox "     \Zr Developed the xDrip team \Zn\n\n\
 You can use this utility only if you use FreeDNS.\n\n\
-Copy the URL shown on the following page.  Paste it into a web browser.  You will have 15 minutes to edit your variables on that browser." 12 50
+Copy the URL shown on the following page.  Paste it into a web browser.  You will have 15 minutes to edit your variables on that browser." 13 50
 clear
 
 sudo /xDrip/ConfigServer/run_server_linux.sh
