c18472946eb47c38eca425856dc78b2a4b5f187f
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-17 00:20:45 -0500

    Create varserver.sh
    Script for the menu to call to run the Python server for editing the variables.

---
+++ varserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dialog --colors --msgbox "     \Zr Developed the xDrip team \Zn\n\n\
+Copy the URL shown on the following page.  Paste it into a web browser.  You will have 15 minutes to edit your variables on that browser." 10 50
+clear
+
+sudo /xDrip/ConfigServer/run_server_linux.sh
+ 
