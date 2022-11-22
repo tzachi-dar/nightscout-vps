f94a1519c11b7378f7275599f0f4227b649a12c7
Yuval Dar <daryuval13@gmail.com>
2022-11-22 19:41:32 +0200

    Add a script to run on windows (development only).

---
+++ ConfigServer/run_server_windows.cmd
@@ -0,0 +1,10 @@
+
+set SECRET_KEY=aaa
+set ENV_DEBUG=True
+set ENV_TOKEN=aaa
+set NS_CONFIG_FILE=example.txt
+set HOSTNAME=snirdev1.mooo.com
+
+pip install django-extensions Werkzeug
+pip install qrcode
+python manage.py runserver
