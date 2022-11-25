8707e0478ed89d2f9bd5d1be5f1459a8f9a9632e
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-24 21:59:15 -0500

    Update menu.sh
    Update the update_scripts.sh file before calling it.

--- menu.sh
+++ menu.sh
@@ -56,6 +56,12 @@ sudo /xDrip/scripts/clone_nightscout.sh
 ;;
 
 7)
+cd /srv
+cd "$(< repo)"  # Go to the local database
+sudo git reset --hard  # delete any local edits.
+sudo git pull  # Update database from remote.
+sudo chmod 755 update_scripts.sh
+sudo cp -f update_scripts.sh /xDrip/scripts/.
 clear
 sudo /xDrip/scripts/update_scripts.sh
 ;;
