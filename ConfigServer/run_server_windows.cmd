b0196b02c9d8a8daf7fb13ea91fe41f55e1321bd
Yuval Dar <daryuval13@gmail.com>
2022-11-25 13:18:14 +0200

    Fix windows script

--- ConfigServer/run_server_windows.cmd
+++ ConfigServer/run_server_windows.cmd
@@ -4,6 +4,7 @@ set ENV_DEBUG=True
 set ENV_TOKEN=aaa
 set NS_CONFIG_FILE=example.txt
 set HOSTNAME=snirdev1.mooo.com
+set APP_JSON_FILE=app.json
 
 pip install django-extensions Werkzeug
 pip install qrcode
