ff2f8d544f168d79f9c4b028d9883c590da93c34
Navid <51497406+Navid200@users.noreply.github.com>
2022-12-08 23:59:45 -0500

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -39,6 +39,11 @@ sudo rm -rf /xDrip/ConfigServer # Remove the existing ConfigServer directory
 sudo cp -r ConfigServer /xDrip/.
 cd ..
 
+# Add log
+rm -rf /tmp/Logs
+echo -e "The platform has been updated     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
+sudo /bin/cp -f /tmp/Logs /xDrip/Logs
+
 if [ ! -s /tmp/nodialog_update_scripts ]
 then
 dialog --colors --msgbox "    \Zr Developed by the xDrip team \Zn\n\n\
