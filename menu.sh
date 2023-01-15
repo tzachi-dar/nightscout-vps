b7023f819a0dd2f9b0617a54b4d85df98b2bd8f8
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-15 16:28:00 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,23 +9,17 @@ do
 
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
-      \Zr Developed by the xDrip team \Zn\
-  \n\n
+        \Zr Developed by the xDrip team \Zn\n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 24 50 14\
+Press Enter to execute the highlighted option.\n\n" 18 50 8\
  "1" "Status"\
- "2" "Installation phase 1 - 15 minutes"\
- "3" "Installation phase 2 - 5 minutes"\
- "4" "Edit variables"\
- "5" "Edit variables in a browser"\
- "6" "Copy data from another Nightscout"\
- "7" "Update scripts"\
- "8" "Backup MongoDB"\
- "9" "Restore MongoDB backup"\
- "10" "FreeDNS Setup"\
- "11" "Customize Nightscout"\
- "12" "Reboot server (Nightscout)"\
- "13" "Exit to shell (terminal)"\
+ "2" "Logs"\
+ "3" "Google Cloud setup"\
+ "4" "Nightscout setup"\
+ "5" "xDrip setup"\
+ "6" "Data"\
+ "7" "Reboot server (Nightscout)"\
+ "8" "Exit to shell (terminal)"\
  3>&1 1>&2 2>&3)
 
 case $Choice in
@@ -35,55 +29,27 @@ case $Choice in
 ;;
 
 2)
-sudo /xDrip/scripts/NS_Install.sh
+clear
+dialog --colors --title "\Zr Developed by the xDrip team \Zn"   --textbox /xDrip/Logs 26 74 
 ;;
 
 3)
-sudo /xDrip/scripts/NS_Install2.sh
+/xDrip/scripts/menu_GC_Setup.sh
 ;;
 
 4)
-/xDrip/scripts/variables.sh
+/xDrip/scripts/menu_NS_setup.sh
 ;;
 
 5)
-/xDrip/scripts/varserver.sh
+/xDrip/scripts/menu_xDripSetup.sh
 ;;
 
 6)
-clear
-sudo /xDrip/scripts/clone_nightscout.sh
+/xDrip/scripts/menu_Data.sh
 ;;
 
 7)
-cd /srv
-cd "$(< repo)"  # Go to the local database
-sudo git reset --hard  # delete any local edits.
-sudo git pull  # Update database from remote.
-sudo chmod 755 update_scripts.sh
-sudo cp -f update_scripts.sh /xDrip/scripts/.
-clear
-sudo /xDrip/scripts/update_scripts.sh
-;;
-
-8)
-/xDrip/scripts/backupmongo.sh
-;;
-
-9)
-/xDrip/scripts/restoremongo.sh
-;;
-
-10)
-clear
-sudo /xDrip/scripts/ConfigureFreedns.sh
-;;
-
-11)
-sudo /xDrip/scripts/update_nightscout.sh
-;;
-
-12)
 dialog --colors --yesno "     \Zr Developed by the xDrip team \Zn\n\n\
 Are you sure you want to reboot the server?\n
 If you do, all unsaved open files will close without saving.\n"  10 50
@@ -96,11 +62,11 @@ sudo reboot
 fi
 ;;
 
-13)
+8)
 cd /tmp
 clear
 dialog --colors --msgbox "        \Zr Developed by the xDrip team \Zn\n\n\
-You will now exit to the shell (terminal).  To return to the menu, enter menu in the terminal." 9 50
+You will now exit to the shell (terminal).  To return to the menu, enter "menu" in the terminal without the quotes." 9 50
 clear
 exit
 ;;
