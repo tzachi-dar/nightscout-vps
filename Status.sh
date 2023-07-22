c6c4c917a18f5024791a011e28e86b6fe710b3d2
Navid <51497406+Navid200@users.noreply.github.com>
2023-07-21 21:27:09 -0400

    Update Status.sh
    Date

--- Status.sh
+++ Status.sh
@@ -163,7 +163,7 @@ Disk size: $disksz        $DiskUsedPercent used \n\
 Ubuntu: $ubuntu \n\
 HTTP & HTTPS:  $http \n\
 ------------------------------------------ \n\
-Google Cloud Nightscout  2023.07.21\n\
+Google Cloud Nightscout  2023.07.22\n\
 $Missing $Phase1 $rclocal_1 $freedns_id_pass \n\n\
 /$uname/$repo/$branch\n\
 Swap: $swap \n\
